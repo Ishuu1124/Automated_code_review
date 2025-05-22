b0c351fc31f0656c8ab3383a1d1ba7fd1e287500
Ayushmaan Singh <ayushmaan.singh+github@iiitb.ac.in>
2025-05-22 20:35:51 +0530

    initial test

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
-from pymilvus import connections
-connections.connect()
-connections.get_connection().is_connected 
+# from pymilvus import connections
+# connections.connect()
+# connections.get_connection().is_connected 
+
+from ibm_watsonx_ai.foundation_models import Embeddings, get_embedding_model_specs
+# from ibm_watsonx_ai.client import 
+from ibm_watsonx_ai.foundation_models.utils.enums import EmbeddingTypes
+
+
