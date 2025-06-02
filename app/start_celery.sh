8c1ef56329d59468e57997c890c9a1050d42fc77
Ayushmaan Singh <ayushmaan.singh@ibm.com>
2025-06-02 13:14:35 +0530

    attempt to fix app behaviour

--- app/start_celery.sh
+++ app/start_celery.sh
@@ -4,4 +4,8 @@ export PYTHONPATH=$(pwd)
 echo $(pwd)
 echo "[$(date)] Initiating start..."
 
-celery -A app.celery_app.worker.celery_app worker --loglevel=info --pool=threads --concurrency=10
+celery -A app.celery_app.worker.celery_app worker --loglevel=info --pool=threads --concurrency=10 &
+
+sleep 30
+
+python -m http.server 8080
